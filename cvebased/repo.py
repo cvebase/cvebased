f4cea726934c41f1817eaa6e40c844e5c23ced27
cve-ai <github@cvebase.com>
2020-11-25 22:12:02 -0700

    compile_cve & add_cve_front_matter, with tests

--- cvebased/repo.py
+++ cvebased/repo.py
@@ -1,18 +1,50 @@
 from ruamel import yaml
 import os
+from cvebased.common import dedupe_sort
 
 
 def compile_researcher(path_to_repo, data):
     filepath = os.path.join(path_to_repo, 'researcher', f"{data['alias']}.md")
-
     # move bio from yaml field to markdown content
     markdown = ''
     if 'bio' in data:
         markdown = data.pop('bio')
-
     write_md(filepath, data, markdown)
-
     return filepath
+
+
+def compile_cve(path_to_repo, data):
+    dirpath = cve_sequence_dir(os.path.join(path_to_repo, 'cve'), data['id'])
+    if not os.path.exists(dirpath):
+        os.makedirs(dirpath)
+    filepath = os.path.join(dirpath, f"{data['id']}.md")
+    # move advisory from yaml field to markdown content
+    markdown = ''
+    if 'advisory' in data:
+        markdown = data.pop('advisory')
+    write_md(filepath, data, markdown)
+    return filepath
+
+
+def add_cve_front_matter(path_to_repo: str, data: dict) -> None:
+    if data['id'] is None:
+        raise Exception("cve id not defined in front matter")
+    exists, path_to_cve = check_cve_exists(path_to_repo, data['id'])
+    if exists:
+        # if file exists, parse existing data and overwrite file
+        with open(path_to_cve, 'r') as file:
+            ex_data, advisory = parse_md(file.read())
+            file.close()
+        del data['id']
+        for k, v in data.items():
+            for el in v:
+                ex_data.setdefault(k, []).append(el)
+            if len(ex_data[k]) > 1:
+                ex_data[k] = dedupe_sort(ex_data[k])
+        write_md(path_to_cve, ex_data)
+    else:
+        # if not exist, generate new file
+        compile_cve(path_to_repo, data)
 
 
 def write_md(filepath, front_matter, markdown=''):
@@ -29,7 +61,7 @@ def write_md(filepath, front_matter, markdown=''):
         file.close()
 
 
-def parse_md(content):
+def parse_md(content: str) -> (dict, str):
     split = content.split('\n---')
     if len(split) < 2:
         raise Exception("error with triple dashes separating front matter")
@@ -44,11 +76,16 @@ def parse_md(content):
     return front_matter, markdown
 
 
-def cve_sequence_dir(base_path, cve):
+def check_cve_exists(path_to_repo: str, cve: str) -> (bool, str):
+    path_to_cve = os.path.join(cve_sequence_dir(os.path.join(path_to_repo, 'cve'), cve), f"{cve}.md")
+    return os.path.exists(path_to_cve), path_to_cve
+
+
+def cve_sequence_dir(path_to_cves: str, cve: str) -> str:
     split = cve.split("-")
     year_dir = "{}".format(split[1])
     sequence_dir = "{}xxx/".format(split[2][:-3])
-    return os.path.join(base_path, year_dir, sequence_dir)
+    return os.path.join(path_to_cves, year_dir, sequence_dir)
 
 
 def scantree(p, ext):
---
+++ tests/test_repo.py
@@ -0,0 +1,46 @@
+import pytest
+import os
+import shutil
+from cvebased.repo import (
+    compile_cve,
+    parse_md,
+    add_cve_front_matter,
+)
+
+
+def test_compile_cve():
+    compile_cve('./tmp', {'id': 'CVE-2020-14882', 'pocs': ['http://example.com/poc']})
+
+    want_filepath = './tmp/cve/2020/14xxx/CVE-2020-14882.md'
+    assert os.path.exists(want_filepath)
+
+    with open(want_filepath, 'r') as f:
+        fm, md = parse_md(f.read())
+        assert fm['id'] == 'CVE-2020-14882'
+        assert fm['pocs'] == ['http://example.com/poc']
+
+    # cleanup
+    shutil.rmtree('./tmp/cve', ignore_errors=True)
+
+
+def test_add_cve_front_matter():
+    # scenario: cve already exists
+    compile_cve('./tmp', {'id': 'CVE-2020-14882', 'pocs': ['http://example.com/poc']})
+    add_cve_front_matter('./tmp', {'id': 'CVE-2020-14882', 'courses': ['http://vulhub.org']})
+
+    want_filepath = './tmp/cve/2020/14xxx/CVE-2020-14882.md'
+    with open(want_filepath, 'r') as f:
+        fm, md = parse_md(f.read())
+        assert fm['id'] == 'CVE-2020-14882'
+        assert fm['courses'] == ['http://vulhub.org']
+
+    # scenario: cve does not yet exist
+    add_cve_front_matter('./tmp', {'id': 'CVE-2020-14883', 'courses': ['http://pentesterlab.com']})
+    want_filepath = './tmp/cve/2020/14xxx/CVE-2020-14883.md'
+    with open(want_filepath, 'r') as f:
+        fm, md = parse_md(f.read())
+        assert fm['id'] == 'CVE-2020-14883'
+        assert fm['courses'] == ['http://pentesterlab.com']
+
+    # cleanup
+    shutil.rmtree('./tmp/cve', ignore_errors=True)
